1219f27efe8f6741ca032f5e1712a78e802166ed
DaoJun <DaoJun@qq.com>
2022-01-14 20:37:18 +0800

    lodash

--- lodash/daojun714298211-lodash.js
+++ lodash/daojun714298211-lodash.js
@@ -60,6 +60,14 @@ var daojun714298211 = {
 
     return res;
   },
+
+  fill: function fill(array, value, start = 0, end = array.length) {
+    for (var i = start; i < end; i++) {
+      array[i] = value;
+    }
+
+    return array;
+  },
 };
 
 // chunk,compact,drop,dropRight,dropRightWhile,dropWhile
