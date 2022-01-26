0753d3a2b88260fbd4aca9b8345d7209ed99d9ca
DaoJun <DaoJun@qq.com>
2022-01-26 22:31:46 +0800

    lodash

--- lodash/daojun714298211-lodash.js
+++ lodash/daojun714298211-lodash.js
@@ -356,7 +356,15 @@ var daojun714298211 = {
 
   sumBy: function sumBy() {},
 
-  sum: function sum() {},
+  sum: function sum(array) {
+    var sum = 0;
+
+    for (var i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+
+    return sum;
+  },
 
   repeat: function repeat() {},
 
